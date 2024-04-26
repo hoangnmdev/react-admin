61483dad9713d1ad8219be94e341966fb4624644
hoangnmdev <hoangnm06122003e@example.com>
2024-04-26 22:01:16 +0700

    side bar p1

--- src/theme.js
+++ src/theme.js
@@ -77,12 +77,12 @@ export const tokens = (mode) => ({
         100: '#040509',
         200: '#080b12',
         300: '#0c101b',
-        400: '#f2f0f0', // manually changed
+        400: '#0000', // manually changed
         500: '#141b2d',
         600: '#1F2A40',
         700: '#727681',
         800: '#a1a4ab',
-        900: '#d0d1d5'
+        900: '#f2f0f0'
       },
       greenAccent: {
         100: '#0f2922',
