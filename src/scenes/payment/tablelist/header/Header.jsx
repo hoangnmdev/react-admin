4e875b4b5ff6ca49470e7dd89d993d15352d0f83
hoangnmdev <hoangnm06122003e@example.com>
2024-05-10 23:53:26 +0700

    implement time in payment dashboard

--- src/scenes/payment/tablelist/header/Header.jsx
+++ src/scenes/payment/tablelist/header/Header.jsx
@@ -1,16 +1,28 @@
 import { Box, useTheme, Typography } from '@mui/material'
 import { tokens } from '~/theme'
 import TableBarIcon from '@mui/icons-material/TableBar'
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
 import { capitalizeLetter } from '~/utils/formatter'
+import { useEffect, useState } from 'react'
+import { displayTime } from '~/utils/time'
 
 const ORDER_TITLE = 'order#:'
 let ORDER_ID = '8797'
 const ORDER_TABLE_TITLE = 'table:'
+const ORDER_TIME_TITLE = 'time:'
 let ORDER_TABLE_NUMBER = 1
 
 function Header() {
-  const theme = useTheme()
-  // const colors = tokens(theme.palette.mode)
+  const [currentTime, setCurrentTime] = useState('')
+  // Update the current time every second
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const timeString = displayTime() // Call the displayCalendar function to get the time string
+      setCurrentTime(timeString) // Update the state with the current time
+    }, 1000)
+    // Cleanup function to clear the interval when component unmounts
+    return () => clearInterval(intervalId)
+  }, [])
 
   return (
     <Box
@@ -26,10 +38,17 @@ function Header() {
         <Typography fontSize={'25px'} ml={'10px'} display={'inline'}>{capitalizeLetter(ORDER_TITLE)}</Typography>
         <Typography fontSize={'25px'} display={'inline'} ml={'5px'}>{ORDER_ID}</Typography>
       </Box>
-      <Box display={'flex'} alignItems={'center'} mr={'20px'}>
-        <TableBarIcon sx={{ fontSize: '20px' }}/>
-        <Typography fontSize={'15px'} color="initial" ml={'10px'} fontWeight={'600'}>{capitalizeLetter(ORDER_TABLE_TITLE)}</Typography>
-        <Typography fontSize={'22px'} fontWeight={'600'} ml={'5px'} color={'#F0B86E'}>{ORDER_TABLE_NUMBER}</Typography>
+      <Box display={'flex'}>
+        <Box display={'flex'} alignItems={'center'}>
+          <TableBarIcon sx={{ fontSize: '20px' }}/>
+          <Typography fontSize={'15px'} color="initial" ml={'5px'} fontWeight={'600'}>{capitalizeLetter(ORDER_TABLE_TITLE)}</Typography>
+          <Typography fontSize={'22px'} fontWeight={'600'} ml={'5px'} color={'#F0B86E'}>{ORDER_TABLE_NUMBER}</Typography>
+        </Box>
+        <Box ml={'15px'} display={'flex'} alignItems={'center'}>
+          <AccessTimeIcon sx={{ fontSize: '20px' }}/>
+          <Typography fontSize={'15px'} color="initial" ml={'5px'} fontWeight={'600'}>{capitalizeLetter(ORDER_TIME_TITLE)}</Typography>
+          <Typography fontSize={'22px'} fontWeight={'600'} ml={'5px'} color={'#F0B86E'}>{currentTime}</Typography>
+        </Box>
       </Box>
     </Box>
   )
