d3061fa8fe60c6e2900f00bc6303ec314e9cce39
hoangnmdev <hoangnm06122003e@example.com>
2024-05-12 23:22:17 +0700

    create header for line and bar chart dashboard

--- src/scenes/linechart/LineChart.jsx
+++ src/scenes/linechart/LineChart.jsx
@@ -1,8 +1,9 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { ResponsiveLine } from '@nivo/line'
+import { capitalizeLetter } from '~/utils/formatter'
 
+const LINE_CHART_TITLE = 'line chart'
 function LineChart() {
-
   const data = [
     {
       'id': 'japan',
@@ -57,226 +58,30 @@ function LineChart() {
           'y': 266
         }
       ]
-    },
-    {
-      'id': 'france',
-      'color': 'hsl(191, 70%, 50%)',
-      'data': [
-        {
-          'x': 'plane',
-          'y': 167
-        },
-        {
-          'x': 'helicopter',
-          'y': 68
-        },
-        {
-          'x': 'boat',
-          'y': 74
-        },
-        {
-          'x': 'train',
-          'y': 251
-        },
-        {
-          'x': 'subway',
-          'y': 241
-        },
-        {
-          'x': 'bus',
-          'y': 225
-        },
-        {
-          'x': 'car',
-          'y': 39
-        },
-        {
-          'x': 'moto',
-          'y': 138
-        },
-        {
-          'x': 'bicycle',
-          'y': 186
-        },
-        {
-          'x': 'horse',
-          'y': 178
-        },
-        {
-          'x': 'skateboard',
-          'y': 115
-        },
-        {
-          'x': 'others',
-          'y': 286
-        }
-      ]
-    },
-    {
-      'id': 'us',
-      'color': 'hsl(120, 70%, 50%)',
-      'data': [
-        {
-          'x': 'plane',
-          'y': 123
-        },
-        {
-          'x': 'helicopter',
-          'y': 48
-        },
-        {
-          'x': 'boat',
-          'y': 55
-        },
-        {
-          'x': 'train',
-          'y': 25
-        },
-        {
-          'x': 'subway',
-          'y': 235
-        },
-        {
-          'x': 'bus',
-          'y': 105
-        },
-        {
-          'x': 'car',
-          'y': 239
-        },
-        {
-          'x': 'moto',
-          'y': 149
-        },
-        {
-          'x': 'bicycle',
-          'y': 253
-        },
-        {
-          'x': 'horse',
-          'y': 194
-        },
-        {
-          'x': 'skateboard',
-          'y': 48
-        },
-        {
-          'x': 'others',
-          'y': 121
-        }
-      ]
-    },
-    {
-      'id': 'germany',
-      'color': 'hsl(188, 70%, 50%)',
-      'data': [
-        {
-          'x': 'plane',
-          'y': 283
-        },
-        {
-          'x': 'helicopter',
-          'y': 13
-        },
-        {
-          'x': 'boat',
-          'y': 268
-        },
-        {
-          'x': 'train',
-          'y': 283
-        },
-        {
-          'x': 'subway',
-          'y': 128
-        },
-        {
-          'x': 'bus',
-          'y': 75
-        },
-        {
-          'x': 'car',
-          'y': 198
-        },
-        {
-          'x': 'moto',
-          'y': 19
-        },
-        {
-          'x': 'bicycle',
-          'y': 31
-        },
-        {
-          'x': 'horse',
-          'y': 209
-        },
-        {
-          'x': 'skateboard',
-          'y': 239
-        },
-        {
-          'x': 'others',
-          'y': 34
-        }
-      ]
-    },
-    {
-      'id': 'norway',
-      'color': 'hsl(303, 70%, 50%)',
-      'data': [
-        {
-          'x': 'plane',
-          'y': 148
-        },
-        {
-          'x': 'helicopter',
-          'y': 184
-        },
-        {
-          'x': 'boat',
-          'y': 174
-        },
-        {
-          'x': 'train',
-          'y': 206
-        },
-        {
-          'x': 'subway',
-          'y': 67
-        },
-        {
-          'x': 'bus',
-          'y': 108
-        },
-        {
-          'x': 'car',
-          'y': 274
-        },
-        {
-          'x': 'moto',
-          'y': 152
-        },
-        {
-          'x': 'bicycle',
-          'y': 39
-        },
-        {
-          'x': 'horse',
-          'y': 118
-        },
-        {
-          'x': 'skateboard',
-          'y': 160
-        },
-        {
-          'x': 'others',
-          'y': 42
-        }
-      ]
     }
   ]
   return (
-    <Box width={'100%'} height={'600px'}>
+    <Box width={'100%'} height={'550px'}>
+      <Box
+        borderTop= '2px solid rgba(0, 0, 0, 0.1)'
+        borderBottom= '2px solid rgba(0, 0, 0, 0.1)'
+        bgcolor={'white'}
+        height={'60px'}
+        display={'flex'}
+        justifyContent={'space-between'}
+      >
+        <Box>
+          <Typography
+            fontSize={'25px'}
+            fontWeight={'600'}
+            p={'12px'}
+            ml={'10px'}
+          >
+            {capitalizeLetter(LINE_CHART_TITLE)}
+          </Typography>
+        </Box>
+      </Box>
+
       <ResponsiveLine
         data={data}
         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
