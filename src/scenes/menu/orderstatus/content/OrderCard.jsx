a856c1171f683b77ff70318a42f0f9df45246550
hoangnmdev <hoangnm06122003e@example.com>
2024-06-13 00:11:24 +0700

    modify_OrderCard_UI

--- src/scenes/menu/orderstatus/content/OrderCard.jsx
+++ src/scenes/menu/orderstatus/content/OrderCard.jsx
@@ -1,134 +1,151 @@
+import { useState } from 'react'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
-import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import DeleteIcon from '@mui/icons-material/Delete'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 
 export default function MediaControlCard() {
+  const [data, setData] = useState([
+    {
+      itemName: 'Cheesy Chicken Pasta',
+      amount: '$100',
+      quantity: 2,
+      price: '70.000Đ'
+    },
+    {
+      itemName: 'Cheesy Chicken Pasta',
+      amount: '$200',
+      quantity: 4,
+      price: '$200.000'
+    },
+    {
+      itemName: 'Cheesy Chicken Pasta',
+      amount: '$75',
+      quantity: 3,
+      price: '$350'
+    },
+    {
+      itemName: 'Cheesy Chicken Pasta',
+      amount: '$200',
+      quantity: 1,
+      price: '$600'
+    }
+  ])
+
+  const handleQuantityChange = (index, change) => {
+    const newData = [...data]
+    const newQuantity = newData[index].quantity + change
+    newData[index].quantity = newQuantity < 1 ? 1 : newQuantity
+    setData(newData)
+  }
 
   return (
-    <Box
-      justifyContent={'center'}
-      justifyItems={'center'}
-      display={'flex'}
-    >
-      <Box sx={{ overflowY: 'auto' }}
+    <Box justifyContent={'center'} justifyItems={'center'} display={'flex'} width={'420px'}>
+      <Box sx={{ overflowY: 'auto', overflowX: 'hidden' }}
         justifyContent={'center'}
         justifyItems={'center'}
         display={'flex'}
-        width={'380px'}
-        height={'200px'}
-        border= '3px dashed rgba(0, 0, 0, 0.1)'
+        width={'480px'}
+        maxWidth={'1000px'}
+        height={'250px'}
+        border='3px dashed rgba(0, 0, 0, 0.1)'
         borderRadius={'10px'}
       >
-        <div style={{ display:  'block' }}>
-          <Card
-            sx={{
-              display: 'flex',
-              justifyContent:'space-between',
-              height: '70px',
-              width: '350px',
-              bgcolor:'#F1F2EB',
-              '&.MuiPaper-root': {
-                boxShadow: 'none'
-              },
-              borderRadius: '15px',
-              mb: '10px'
-            }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <CardMedia
-                component="img"
-                sx={{
-                  width: 55,
-                  height: 55,
-                  m: ' 4px 0 4px 12px',
-                  borderRadius:'8px'
-                }}
-                image="https://thumbs.dreamstime.com/z/white-chocolate-chip-cookies-keyboard-computer-background-copy-space-website-internet-homepage-policy-accpeted-blocks-252209597.jpg?ct=jpeg"
-                alt="Live from space album cover"
-              />
-              <Box pl ={'18px'}>
-                <Typography fontSize={'13px'} color="initial" fontWeight={'510'}>CHICKEN WINGS</Typography>
-                <Typography variant='h2' fontWeight={'600'}>$10</Typography>
+        <div style={{ display: 'block', width: '100%' }}>
+          {data.map((item, index) => (
+            <Card
+              key={index}
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                height: '60px',
+                width: '100%',
+                bgcolor: '#F1F2EB',
+                '&.MuiPaper-root': {
+                  boxShadow: 'none'
+                },
+                borderRadius: '10px',
+                mb: '10px',
+                p: '10px'
+              }}
+            >
+              <Box display={'inline-block'}>
+                <Box display={'flex'} alignItems={'center'}>
+                  <Typography fontSize={'14px'} color="initial" fontWeight={'510'}>
+                    {item.itemName}
+                  </Typography>
+                  <IconButton size="small" sx={{ p: 0.5 }}>
+                    <OpenInNewIcon sx={{ fontSize: '15px' }} />
+                  </IconButton>
+                </Box>
+                <Typography fontSize={'12px'} color="initial" fontWeight={'600'}>
+                  Amount {item.amount}
+                </Typography>
               </Box>
-            </Box>
-            <Box textAlign={'center'} sx={{ m: ' 4px 20px 4px 12px' }}>
-              <Typography fontSize={'13px'} fontWeight={'510'} color="initial">QUANTITY</Typography>
-              <Typography variant='h2'fontWeight={'600'}>1</Typography>
-            </Box>
-          </Card>
-          <Card
-            sx={{
-              display: 'flex',
-              justifyContent:'space-between',
-              height: '70px',
-              width: '350px',
-              bgcolor:'#F1F2EB',
-              '&.MuiPaper-root': {
-                boxShadow: 'none'
-              },
-              borderRadius: '15px',
-              mb: '10px'
-            }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <CardMedia
-                component="img"
-                sx={{
-                  width: 55,
-                  height: 55,
-                  m: ' 4px 0 4px 12px',
-                  borderRadius:'8px'
-                }}
-                image="https://thumbs.dreamstime.com/z/white-chocolate-chip-cookies-keyboard-computer-background-copy-space-website-internet-homepage-policy-accpeted-blocks-252209597.jpg?ct=jpeg"
-                alt="Live from space album cover"
-              />
-              <Box pl ={'18px'}>
-                <Typography fontSize={'13px'} color="initial" fontWeight={'510'}>CHICKEN WINGS</Typography>
-                <Typography variant='h2' fontWeight={'600'}>$10</Typography>
+              <Box textAlign={'center'} sx={{ display: 'flex', alignItems: 'center', bgcolor: '#4F6F52', borderRadius: '5px', marginLeft: 'auto' }}>
+                <Button
+                  onClick={() => handleQuantityChange(index, -1)}
+                  variant="outlined"
+                  size="small"
+                  sx={{
+                    minWidth: '20px',
+                    borderRadius: '5px',
+                    borderStyle: 'none',
+                    '&:hover':{
+                      bgcolor: '#4F6F52',
+                      boxShadow: 'rgba(0, 0, 0, 0.25) 0 2px 10px',
+                      transform: 'translateY(-2px)',
+                      '& .MuiTypography-root': {
+                        color: 'white'
+                      },
+                      '&:active': {
+                        boxShadow: 'none',
+                        transform: 'translateY(0)'
+                      }
+                    }
+                  }}>
+                  <Typography color={'white'}>-</Typography>
+                </Button>
+                <Typography color={'white'} mx={2}>{item.quantity}</Typography>
+                <Button
+                  onClick={() => handleQuantityChange(index, 1)}
+                  variant="outlined"
+                  size="small"
+                  sx={{
+                    minWidth: '20px',
+                    borderRadius: '5px',
+                    borderStyle: 'none',
+                    '&:hover':{
+                      bgcolor: '#4F6F52',
+                      boxShadow: 'rgba(0, 0, 0, 0.25) 0 2px 10px',
+                      transform: 'translateY(-2px)',
+                      '& .MuiTypography-root': {
+                        color: 'white'
+                      },
+                      '&:active': {
+                        boxShadow: 'none',
+                        transform: 'translateY(0)'
+                      }
+                    }
+                  }}>
+                  <Typography color={'white'}>+</Typography>
+                </Button>
               </Box>
-            </Box>
-            <Box textAlign={'center'} sx={{ m: ' 4px 20px 4px 12px' }}>
-              <Typography fontSize={'13px'} fontWeight={'510'} color="initial">QUANTITY</Typography>
-              <Typography variant='h2'fontWeight={'600'}>1</Typography>
-            </Box>
-          </Card>
-          <Card
-            sx={{
-              display: 'flex',
-              justifyContent:'space-between',
-              height: '70px',
-              width: '350px',
-              bgcolor:'#F1F2EB',
-              '&.MuiPaper-root': {
-                boxShadow: 'none'
-              },
-              borderRadius: '15px',
-              mb: '10px'
-            }}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <CardMedia
-                component="img"
-                sx={{
-                  width: 55,
-                  height: 55,
-                  m: ' 4px 0 4px 12px',
-                  borderRadius:'8px'
-                }}
-                image="https://pho10lyquocsu.com.vn/watermark/product/540x540x1/upload/product/febddb544adeb480edcf-3155.jpg"
-                alt="Live from space album cover"
-              />
-              <Box pl ={'18px'}>
-                <Typography fontSize={'13px'} color="initial" fontWeight={'510'}>CHICKEN WINGS</Typography>
-                <Typography variant='h2' fontWeight={'600'}>$10</Typography>
+              <Box justifyContent={'space-between'} display={'flex'} alignItems={'center'} ml={'auto'}>
+                <Typography variant='h6' fontWeight={'600'} sx={{ minWidth: '80px', textAlign: 'center' }}>
+                  {item.price}
+                </Typography>
+                <IconButton color="error">
+                  <DeleteIcon />
+                </IconButton>
               </Box>
-            </Box>
-            <Box textAlign={'center'} sx={{ m: ' 4px 20px 4px 12px' }}>
-              <Typography fontSize={'13px'} fontWeight={'510'} color="initial">QUANTITY</Typography>
-              <Typography variant='h2'fontWeight={'600'}>1</Typography>
-            </Box>
-          </Card>
+            </Card>
+          ))}
         </div>
       </Box>
     </Box>
-
   )
 }
